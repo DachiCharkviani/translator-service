86c44d8767e3b9b7604eb58015854de638fc3912
Dachi <user@MacBook-Air-24.local>
2024-11-18 14:55:16 -0800

    changed the files to match exact translations

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,13 +1,13 @@
-from src.translator import translate_content
+from src.translator import get_translation, get_language_with_retry
 
-
-def test_chinese():
-    is_english, translated_content = translate_content("这是一条中文消息")
-    assert is_english == False
-    assert translated_content == "This is a Chinese message"
+# def test_chinese():
+#     is_english, translated_content = get_translation("这是一条中文消息")
+#     assert is_english == False
+#     assert translated_content == "This is a Chinese message"
 
 def test_llm_normal_response():
-    pass
+    assert get_translation("Hier ist dein erstes Beispiel.") == 'Here is your first example.'
+    assert get_language_with_retry("Hier ist dein erstes Beispiel.") == "German"
 
 def test_llm_gibberish_response():
     pass
